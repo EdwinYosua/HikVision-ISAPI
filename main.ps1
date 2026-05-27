38669a52b962f8bbd6b64724a4a5527dc13190b9
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2026-05-27 15:53:54 +0800

    Update main.ps1
    easier directory setup for config.csv files

--- main.ps1
+++ main.ps1
@@ -1,10 +1,10 @@
-$nvrList = Import-Csv "E:\ISAPI\config.csv"
+$nvrList = Import-Csv "$PSScriptRoot\nvr_config.csv"
 
 foreach ($nvr in $nvrList) {
 
     # Write-Host "Connecting to $($nvr.IP)..."
 
-    & "E:\ISAPI\extractNVR.ps1" `
+    & "$PSScriptRoot\extractNVR.ps1" `
         -NVR_IP $nvr.IP `
         -User $nvr.Username `
         -Pass $nvr.Password `
